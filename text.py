cf1b53a0e18bbe9ae7a0c7c29843a1444380d7cf
gaochuqi <64792208+gaochuqi@users.noreply.github.com>
2022-03-10 18:38:25 +0800

    Update text.py

--- text.py
+++ text.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
 
     # Test which classes performed well
     # testAccuracy()
-    model = MainDenoise(cfg)
+    model = MainDenoise()
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = model.to(device)
     PATH= cfg.model_save_root
