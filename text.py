27349b325ca04d603e4741a2ca475af822fcf011
gaochuqi <64792208+gaochuqi@users.noreply.github.com>
2022-03-10 17:54:02 +0800

    Update text.py

--- text.py
+++ text.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
 
     # Test which classes performed well
     # testAccuracy()
-    model = arch_qat.EMVD(cfg)
+    model = MainDenoise(cfg)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = model.to(device)
     PATH= cfg.model_save_root
